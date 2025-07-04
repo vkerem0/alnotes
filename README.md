4882d0c14bb33d8ddf0fe1c271be61a831b90e14
Alnes <aln@gnu.org>
2025-07-04 22:29:07 +0300

    Readme added. Theme globalized.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# alnotes
+## Al Notes
+Take your notes more sigma way! 
+<hr>
+<img src="alnotes_ui.png" alt="Screenshot of Al Notes">
+<i>Screenshot</i>
+
+## Licence
+
+This software is licensed under the **LGPL-2.1**.<br>
+You can use it for **commercial** purposes.<br>
+For more details, please read the **LICENCE** file<br>
+
+<img src="icoo.png" width='200' alt="devs">
+made with Qt by Kerem & Alnes
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,8 +5,38 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+// Global Dark Theme
+    QString UI_Style = R"(
+QTextEdit{
 
+background-color:rgb(50, 50, 50);
+
+}
+
+QTreeView{
+
+background-color: rgb(50, 50, 50);
+border:none;
+}
+QToolButton{
+
+background-color: rgb(50, 50,50);
+border:none;
+
+}
+
+QToolButton:hover{
+background-color: rgb(65, 65, 65);
+}
+
+QToolButton:pressed {
+
+background-color: rgb(31, 31, 31);
+
+}
+)";
+    qApp->setStyleSheet(UI_Style);
+    ui->setupUi(this);
 
 
     QDir dir(appDirPath);
@@ -22,6 +52,7 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+
 }
 
 
